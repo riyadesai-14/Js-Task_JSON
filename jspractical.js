ecd975089ccb5925310e31719cfd88d983135af4
Nikita Vyas <nikita.vyas@bytestechnolab.com>
2022-11-15 17:31:41 +0530

    first commit

---
+++ jspractical.js
@@ -0,0 +1,153 @@
+/*
+//1. Write a JavaScript function to check whether an `input` is an array or not.
+arr=[1,22,4];
+
+//2.Write a simple JavaScript program to join all elements of the following array into a string. 
+const myColor = ["Red", "Green", "White", "Black"];
+
+//3.Write a JavaScript program to sort the items of an array. 
+var arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+
+//4.Write a JavaScript program to find the most frequent item of an array. 
+var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+
+//5.Write a JavaScript program which accept a string as input and swap the case of each character
+let str= 'THe Quick BrOwn Fox';
+
+//6.Write a JavaScript program which prints the elements of the following array.
+var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+//7.write javascript function to return sum of given array
+// const arr= [1,2,4,6,7,3,6];
+
+//8.Write a JavaScript function to remove. 'null', '0', '""', 'false', 'undefined' and 'NaN' values from an array.
+let sample= [NaN, 0, 15, false, -22, '',undefined, 47, null];
+
+//9.Write a JavaScript function to sort the following array of objects by title value.
+var library = [ 
+    { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+    { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+    { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
+];
+
+//10.Write a JavaScript function to merge two arrays and removes all duplicates elements
+var array1 = [1, 2, 3];
+var array2 = [2, 30, 1];
+
+//11.Write a JavaScript function to remove a specific element from an array
+let array= [2, 5, 9, 6, 5,12, 13, 1, 5];
+
+//12.Write a JavaScript function to find an array contains a specific element
+let array= [2, 5, 9, 6];
+
+//13.Write a JavaScript function to get a random item from an array
+let array= [2, 5, 9, 6];
+random = (array)=>{
+    return array[Math.floor(Math.random()*array.length)];  
+}
+console.log(random(array));
+*/
+
+/*
+//14.Write a JavaScript function to move an array element from one position to another
+let arr= [10, 20, 30, 40, 50];
+
+//15.Write a function that compares two arrays and returns true if they are identical.
+const arr1 = [1, 2, 3, 4];
+const arr2 = [1, 2, 3, 4];
+const arr3 = [1, 2, 3, 5];
+const arr4 = [1, 2, 3, 4, 5];
+
+
+/*
+//16.create function which have age is 18
+var library = [ 
+    { author: 'Bill Gates', title: 'The Road Ahead', age: '18'},
+    { author: 'Steve Jobs', title: 'Walter Isaacson', age: 18},
+    { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', age: 18}
+    ];
+
+
+//17. Write a JavaScript program to display the current day and time in the following format.
+
+//18.Write a JavaScript program to get the current date.
+
+
+
+//19.Write a JavaScript program where the program takes a random integer between 1 to 10, the user is then prompted to input a guess number. If the user input matches with guess number, the program will display a message "Good Work" otherwise display a message "Not matched"
+
+//20.Write a JavaScript exercise to get the extension of a filename.
+let filename = "javascripttest.js";
+
+//22.Write a JavaScript program to find the largest of three given integers.
+let num1= 824;
+let num2= 2221;
+let num3= 8171;
+
+
+//23.Write a program to check whether a specified character exists within the 2nd to 4th position in a given string.
+
+//24.Write a JavaScript program to list the properties of a JavaScript object.
+var student = {
+    name : "David Rayy",
+    sclass : "VI",
+    rollno : 12 
+};
+
+//25) Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
+var student = {
+    name : "David Rayy",
+    sclass : "VI",
+    rollno : 12 
+};
+
+//26.Write a JavaScript program to get the length of a JavaScript object.
+var student = {
+    name : "David Rayy",
+    sclass : "VI",
+    rollno : 12 
+};
+
+//27. Write a JavaScript function to convert an object into a list of `[key, value]` pairs
+var student = {
+    name : "David Rayy",
+    sclass : "VI",
+    rollno : 12 
+};
+
+
+//28.Write a JavaScript function to split a string and convert it into an array of words
+let data= "Robin Singh";
+
+//29.Write a JavaScript function to parameterize a string.
+let data= 'Robin Singh from USA'
+
+//30.Write a JavaScript function check if a string ends with specified suffix
+let data= 'JS PHP PYTHON';
+
+
+
+//31.Write a JavaScript program to sort an array of objects, ordered by properties and orders
+// const users = [
+//     { name: 'fred', age: 48 },
+//     { name: 'barney', age: 36 },
+//     { name: 'fred', age: 40 },
+// ];
+
+// console.log(orderBy(users, ['name', 'age'], ['asc', 'desc']));
+// console.log(orderBy(users, ['name', 'age']));
+
+/*
+//32. Given a string, reverse each word in the sentence
+let str= "Welcome to this Javascript Guide!";
+
+
+//33.empty array
+
+//34.number 12.2 is integer or not ? give answer using program
+
+//35.[1, 2, 3, 4, 5] duplicate this array
+
+//36.Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, "buzz" at multiples of 5 and "fizzbuzz" at multiples of 3 and 5.
+
+
